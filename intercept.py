f485316cb8365071e05510071f80a4cf4b179938
Kelvin <kelvinpella@gmail.com>
2020-10-21 22:47:33 +0300

    data read from excel

---
+++ intercept.py
@@ -0,0 +1,28 @@
+import xlrd
+
+def excel_file(filename):
+    if filename.endswith(('.xlsx','.xls', '.csv')):
+        # open excel and sheet
+        workbook = xlrd.open_workbook(filename)
+        sheet = workbook.sheet_by_index(0)
+        
+        # Stores Position and Partcode value pairs from all rows.
+        all_data = []
+
+        # loop over rows and columns to extract Position and Partcode pairs
+        for row in range(sheet.nrows):
+            current_row = []
+            for col in range(sheet.ncols):
+                if col == 1 or col == 3:
+                    data = sheet.cell_value(row, col)
+                    new_data = data.replace('/',"_") # useful when searching in CC
+                    current_row.append(new_data)
+            all_data.append(current_row)        
+
+        print(all_data)
+
+    else:
+        print(f'{filename} is not excel file.')
+
+if __name__ == "__main__":
+    excel_file(filename = 'excel.xlsx')        
