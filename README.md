25fdb05676e7a9c662b64c03896a9a511d6add5f
Kelvin <kelvinpella@gmail.com>
2020-10-21 23:15:36 +0300

    changes to readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # INTERCEPT 
-This program will be used for automatically reading and extracting excel values(Partcodes and IDs), then will search for matching elements in Catia composer and assign respective IDs.
+This program will be used for automatically reading and extracting data values(Partcodes and IDs) from excel sheets, then will search for matching elements in Catia composer and assign respective IDs.
--- intercept.py
+++ intercept.py
@@ -1,11 +1,12 @@
 import xlrd
 
+
 def excel_file(filename):
-    if filename.endswith(('.xlsx','.xls', '.csv')):
+    if filename.endswith(('.xlsx', '.xls', '.csv')):
         # open excel and sheet
         workbook = xlrd.open_workbook(filename)
         sheet = workbook.sheet_by_index(0)
-        
+
         # Stores Position and Partcode value pairs from all rows.
         all_data = []
 
@@ -15,14 +16,16 @@ def excel_file(filename):
             for col in range(sheet.ncols):
                 if col == 1 or col == 3:
                     data = sheet.cell_value(row, col)
-                    new_data = data.replace('/',"_") # useful when searching in CC
+                    # useful when searching in CC
+                    new_data = data.replace('/', "_")
                     current_row.append(new_data)
-            all_data.append(current_row)        
+            all_data.append(current_row)
 
         print(all_data)
 
     else:
         print(f'{filename} is not excel file.')
 
+
 if __name__ == "__main__":
-    excel_file(filename = 'excel.xlsx')        
+    excel_file(filename='excel.xlsx')
