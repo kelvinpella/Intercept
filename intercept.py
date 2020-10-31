a1cd1b7b17dfc6306742f724c74ccc2361c8447d
Kelvin <kelvinpella@gmail.com>
2020-10-31 19:35:41 +0300

    modified app to Class

--- intercept.py
+++ intercept.py
@@ -1,59 +1,78 @@
 import xlrd
+import time
 import ntpath
 import pyautogui
 from pyscreeze import ImageNotFoundException
 from tkinter import *
+from tkinter import ttk
 from tkinter import filedialog
 
-def inside_catia(data):
-    # check if catia document is opened
-    try:
-        axis = pyautogui.locateCenterOnScreen('images/catia.PNG')
-        print(axis)
-    except ImageNotFoundException:
-        print("Catia not opened.")  
+class App:
 
-def excel_file():
-    filepath = filedialog.askopenfilename(initialdir="/Desktop",title='Select File',filetypes=[("Excel files", ".xlsx .xls .csv")])
-    filename = ntpath.basename(filepath)
-    if filename.endswith(('.xlsx', '.xls', '.csv')):
-        # open excel and sheet
-        workbook = xlrd.open_workbook(filepath)
-        sheet = workbook.sheet_by_index(0)
+    def __init__(self,parent):
+        self.parent = parent
+        self.frame = Frame(parent).place(relwidth=1,relheight=1)
+        self.upload_label = Label(self.frame,text="Upload Excel file.",font=("Helvetica", 15))
+        self.upload_label.place(x=10,y=10)
+        self.upload_button = Button(self.frame,text="Upload",font=("Helvetica", 15),command = self.excel_file)
+        self.upload_button.place(width=180,height=50,x=10,y=50)
 
-        # Stores Position and Partcode value pairs from all rows.
-        all_data = []
+    def inside_catia(self,data):
+        # check if catia document is opened
+        try:
+            axis = pyautogui.locateCenterOnScreen('images/catia.PNG')
+            print(axis)
+        except ImageNotFoundException:
+            print("Catia not opened.")  
 
-        # loop over rows and columns to extract Position and Partcode pairs
-        for row in range(sheet.nrows):
-            current_row = []
-            for col in range(sheet.ncols):
-                if col == 1 or col == 3:
-                    data = sheet.cell_value(row, col)
-                    # useful when searching in CC
-                    new_data = data.replace('/', "_")
-                    current_row.append(new_data)
-            all_data.append(current_row)
+    def excel_file(self):
+        filepath = filedialog.askopenfilename(initialdir="/Desktop",title='Select File',filetypes=[("Excel files", ".xlsx .xls .csv")])
+        filename = ntpath.basename(filepath)
+        self.upload_label.destroy()
+        self.upload_button.destroy()
+        if filename.endswith(('.xlsx', '.xls', '.csv')):
+            upload_progress = Label(self.frame,text="File uploading",font=("Helvetica", 15))
+            upload_progress.pack(side='left',padx=10)
+            row_progress = ttk.Progressbar(self.frame,orient= HORIZONTAL,length=300,mode="determinate")
+            row_progress.pack(side='left')
+            # open excel and sheet
+            workbook = xlrd.open_workbook(filepath)
+            sheet = workbook.sheet_by_index(0)
 
-        # inside_catia(all_data)
-        print(all_data)
-        return filename
+            # Stores Position and Partcode value pairs from all rows.
+            all_data = []
 
-    else:
-        print(f'{filename} is not excel file.')
-        return filename
+            # loop over rows and columns to extract Position and Partcode pairs
+            for row in range(sheet.nrows):
+                current_row = []
+                row_progress['value'] += 100 / sheet.nrows
+                self.parent.update_idletasks()
+                time.sleep(0.001)
+                for col in range(sheet.ncols):
+                    if col == 1 or col == 3:
+                        data = sheet.cell_value(row, col)
+                        # useful when searching in Catia Composer
+                        new_data = data.replace('/', "_")
+                        current_row.append(new_data)
+                all_data.append(current_row)
+
+            # inside_catia(all_data)
+            print(all_data)
+            return filename
+
+        else:
+            print(f'{filename} is not excel file.')
+            return filename
+
 def main():
-    upload_label = Label(frame,text="Upload Excel file.",font=("Helvetica", 15)).place(x=10,y=10)
-    upload_button = Button(frame,text="Upload",font=("Helvetica", 15),command = excel_file).place(width=180,height=50,x=10,y=50)
-            
-
-if __name__ == "__main__":
     app = Tk()
     app.wm_iconbitmap('images/intercept.ico')
     app.wm_title('INTERCEPT')
-    canvas = Canvas(app,height=120, width=600).pack()
-    frame = Frame(app).place(relwidth=1,relheight=1)
-    main()
+    app.geometry("600x120")
+    application = App(app)
     app.mainloop()
+
+if __name__ == "__main__":
+    main()
     
     
