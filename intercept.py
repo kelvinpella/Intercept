3f04417a62f31ebb737aba391ac368697b9c45a3
Kelvin <kelvinpella@gmail.com>
2020-11-01 16:36:30 +0300

    File errors handled

--- intercept.py
+++ intercept.py
@@ -4,8 +4,7 @@ import ntpath
 import pyautogui
 from pyscreeze import ImageNotFoundException
 from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
+from tkinter import ttk,filedialog,messagebox
 
 class App:
 
@@ -31,43 +30,48 @@ class App:
         self.excel_file()
 
     def excel_file(self):
-        filepath = filedialog.askopenfilename(initialdir="/Desktop",title='Select File',filetypes=[("Excel files", ".xlsx .xls .csv")])
-        filename = ntpath.basename(filepath)
-        if filename.endswith(('.xlsx', '.xls', '.csv')):
-            upload_progress = Label(self.frame,text="File uploading",font=("Helvetica", 13))
-            upload_progress.pack(side='left',padx=10)
-            row_progress = ttk.Progressbar(self.frame,orient= HORIZONTAL,length=300,mode="determinate")
-            row_progress.pack(side='left')
-            # open excel and sheet
-            workbook = xlrd.open_workbook(filepath)
-            sheet = workbook.sheet_by_index(0)
-
-            # Stores Position and Partcode value pairs from all rows.
-            all_data = []
-
-            # loop over rows and columns to extract Position and Partcode pairs
-            for row in range(sheet.nrows):
-                current_row = []
-                row_progress['value'] += 100 / sheet.nrows
-                self.parent.update_idletasks()
-                time.sleep(0.01)
-                for col in range(sheet.ncols):
-                    if col == 1 or col == 3:
-                        data = sheet.cell_value(row, col)
-                        # useful when searching in Catia Composer
-                        new_data = data.replace('/', "_")
-                        current_row.append(new_data)
-                all_data.append(current_row)
-
-            # inside_catia(all_data)
-            print(all_data)
-            return filename
-
+        filepath = filedialog.askopenfilename(initialdir="/Desktop",title='Select File')#,filetypes=[("Excel files", ".xlsx .xls .csv")])
+        
+        if not filepath:
+            App(app)
         else:
-            print(f'{filename} is not excel file.')
-            return filename
+            filename = ntpath.basename(filepath)
+            if filename.endswith(('.xlsx', '.xls', '.csv')):
+                upload_progress = Label(self.frame,text="File uploading",font=("Helvetica", 13))
+                upload_progress.pack(side='left',padx=10)
+                row_progress = ttk.Progressbar(self.frame,orient= HORIZONTAL,length=300,mode="determinate")
+                row_progress.pack(side='left')
+                # open excel and sheet
+                workbook = xlrd.open_workbook(filepath)
+                sheet = workbook.sheet_by_index(0)
+
+                # Stores Position and Partcode value pairs from all rows.
+                all_data = []
+
+                # loop over rows and columns to extract Position and Partcode pairs
+                for row in range(sheet.nrows):
+                    current_row = []
+                    row_progress['value'] += 100 / sheet.nrows
+                    self.parent.update_idletasks()
+                    time.sleep(0.01)
+                    for col in range(sheet.ncols):
+                        if col == 1 or col == 3:
+                            data = sheet.cell_value(row, col)
+                            # useful when searching in Catia Composer
+                            new_data = data.replace('/', "_")
+                            current_row.append(new_data)
+                    all_data.append(current_row)
+
+                # inside_catia(all_data)
+                print(all_data)
+                return filename
+
+            else:
+                messagebox.showerror('Error!',f"{filename} is not an excel file.")
+                self.excel_file()
 
 def main():
+    global app
     app = Tk()
     app.wm_iconbitmap('images/intercept.ico')
     app.wm_title('INTERCEPT')
