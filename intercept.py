63deb5b894b6fa3520199ec30dbddcb0e6b3871a
Kelvin <kelvinpella@gmail.com>
2020-11-03 00:10:17 +0300

    file upload success

--- intercept.py
+++ intercept.py
@@ -4,25 +4,34 @@ import ntpath
 import pyautogui
 from pyscreeze import ImageNotFoundException
 from tkinter import *
-from tkinter import ttk,filedialog,messagebox
+from tkinter import ttk, filedialog, messagebox
+
 
 class FileView:
 
-    def __init__(self,parent):
+    def __init__(self, parent):
         self.parent = parent
-        self.frame = Frame(parent).place(relwidth=1,relheight=1)
-        self.upload_label = Label(self.frame,text="Upload Excel file.",font=("Helvetica", 13))
-        self.upload_label.place(x=10,y=10)
-        self.upload_button = Button(self.frame,text="Upload",font=("Helvetica", 13),command = self.destroy_widget)
-        self.upload_button.place(width=180,height=50,x=10,y=50)
+        self.frame = Frame(parent).place(relwidth=1, relheight=1)
+        self.upload_label = Label(
+            self.frame, text="Upload Excel file.", font=("Helvetica", 13))
+        self.upload_label.place(x=10, y=10)
+        self.upload_button = Button(self.frame, text="Upload", font=(
+            "Helvetica", 13), command=self.destroy_widget)
+        self.upload_button.place(width=180, height=50, x=10, y=50)
 
-    def inside_catia(self,data):
+    def inside_catia(self, data):
         # check if catia document is opened
         try:
             axis = pyautogui.locateCenterOnScreen('images/catia.PNG')
             print(axis)
         except ImageNotFoundException:
-            print("Catia not opened.")  
+            print("Catia not opened.")
+
+    def notice(self):
+        self.parent.geometry("600x200")
+        notice = Message(
+            self.frame,font=("Helvetica", 10), text="IMPORTANT NOTICE.\n-Make sure required 3D is open in Catia Composer.\n-Make sure no other apps block Catia Composer's screen.\n-Click 'OK' to continue.")
+        notice.pack(side='left')
 
     def destroy_widget(self):
         self.upload_label.destroy()
@@ -30,14 +39,17 @@ class FileView:
         self.excel_file()
 
     def excel_file(self):
-        filepath = filedialog.askopenfilename(initialdir="/Desktop",title='Select File',filetypes=[("Excel files", ".xlsx .xls .csv")])
+        filepath = filedialog.askopenfilename(
+            initialdir="/Desktop", title='Select File', filetypes=[("Excel files", ".xlsx .xls .csv")])
         if not filepath:
             FileView(app)
         else:
             filename = ntpath.basename(filepath)
             if filename.lower().endswith(('.xlsx', '.xls', '.csv')):
-                upload_progress = Label(self.frame,text="File uploading",font=("Helvetica", 13))
-                row_progress = ttk.Progressbar(self.frame,orient= HORIZONTAL,length=300,mode="determinate")
+                upload_progress = Label(
+                    self.frame, text="File uploading", font=("Helvetica", 13))
+                row_progress = ttk.Progressbar(
+                    self.frame, orient=HORIZONTAL, length=300, mode="determinate")
                 # open excel and sheet
                 workbook = xlrd.open_workbook(filepath)
                 sheet = workbook.sheet_by_index(0)
@@ -48,7 +60,7 @@ class FileView:
                 # loop over rows and columns to extract Position and Partcode pairs
                 for row in range(sheet.nrows):
                     current_row = []
-                    upload_progress.pack(side='left',padx=10)
+                    upload_progress.pack(side='left', padx=10)
                     row_progress.pack(side='left')
                     row_progress['value'] += 100 / sheet.nrows
                     self.parent.update_idletasks()
@@ -61,17 +73,20 @@ class FileView:
                             current_row.append(new_data)
                     all_data.append(current_row)
                 if not len(all_data):
-                    messagebox.showerror('Error!',f"{filename} is empty.")
+                    messagebox.showerror('Error!', f"{filename} is empty.")
                     self.excel_file()
-                else:        
-                    upload_progress.config(text='File uploaded successfully.', fg='green')
+                else:
                     row_progress.destroy()
-                    print(all_data)
-                    return all_data
+                    upload_progress.destroy()
+                    messagebox.showinfo(
+                        'Success', 'FILE UPLOADED\n\nClick "OK" to continue.')
+                    self.notice()
 
             else:
-                messagebox.showerror('Error!',f"{filename} is not an Excel file.")
+                messagebox.showerror(
+                    'Error!', f"{filename} is not an Excel file.")
                 self.excel_file()
+
 
 def main():
     global app
@@ -82,7 +97,6 @@ def main():
     application = FileView(app)
     app.mainloop()
 
+
 if __name__ == "__main__":
     main()
-    
-    
